10bffe427ccd0dd9aface644b3044e868a74f61f
DenisLatyshau <denislatyshau@yandex.ru>
2022-10-31 21:52:35 +0300

    continued js

---
+++ scripts/fade-slider.js
@@ -0,0 +1,12 @@
+(function() {
+  
+  const slides = document.querySelectorAll('.fade-slider__item');
+  const activeClass = "fade-slider__item--visible";
+  let index = 0;
+
+  setInterval(function() {
+    slides[index % 3].classList.remove(activeClass);
+    index++
+    slides[index % 3].classList.add(activeClass);
+  }, 5000);
+})();
